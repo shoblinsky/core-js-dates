ac81b19a25d06714b30a32dc5ee35611016a4097
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-01-06 03:15:14 +0500

    feat: getCountDaysInMonth is done

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -98,8 +98,9 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const date = new Date(year, month, 0);
+  return date.getDate();
 }
 
 /**
