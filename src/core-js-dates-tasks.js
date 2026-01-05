773335fbb8fee419731a1b59559436a23f1adce4
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-01-06 03:27:37 +0500

    feat: isDateInPeriod is passing

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -140,8 +140,18 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const start = new Date(period.start);
+  const emd = new Date(period.end);
+
+  const targetDate = new Date(date);
+
+  const startTime = start.getTime();
+  const endTime = emd.getTime();
+
+  const targetTime = targetDate.getTime();
+
+  return targetTime <= endTime && targetTime >= startTime;
 }
 
 /**
